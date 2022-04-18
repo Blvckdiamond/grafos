b9cffb3f3a627bed9708cce6bc838c1017af4d21
Usuario <Usuario@DESKTOP-0BVM1BM>
2022-04-17 21:53:44 -0400

    Agregar archivos de proyecto.

---
+++ grafos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafos
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Vertice oVertice1 = new Vertice(1);
+            Vertice oVertice2 = new Vertice(2);
+            Vertice oVertice3 = new Vertice(3);
+            Vertice oVertice4 = new Vertice(4);
+            Vertice oVertice5 = new Vertice(5);
+            Vertice oVertice6 = new Vertice(6);
+        
+            oVertice6.Aristas.Add(oVertice4);
+            oVertice4.Aristas.Add(oVertice5);
+            oVertice4.Aristas.Add(oVertice3);
+            oVertice3.Aristas.Add(oVertice2);
+            oVertice5.Aristas.Add(oVertice2);
+            oVertice5.Aristas.Add(oVertice1);
+            oVertice2.Aristas.Add(oVertice1);
+
+            Camino(oVertice6);
+        }
+
+        public static void Camino(Vertice oVertice, string sangria="")
+        {
+            if (oVertice != null)
+            {
+                Console.WriteLine(sangria+oVertice.Valor);
+                foreach (var oV in oVertice.Aristas)
+                {
+                    Camino(oV,sangria+"\t");
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ grafos/Vertice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafos
+{
+    public class Vertice
+    {
+        public int Valor { get; set; }
+
+        public List<Vertice> Aristas { get; set; }
+
+        public Vertice(int Valor)
+        {
+            this.Valor = Valor;
+            Aristas = new List<Vertice>();
+        }
+
+
+
+
+    }
+}
+
